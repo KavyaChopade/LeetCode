d59d47f793673949cb93f24f4c303eca9d948a70
Kavya Chopade <167448474+KavyaChopade@users.noreply.github.com>
2025-08-26 19:15:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (66.78%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -19,6 +19,15 @@ class Solution {
         else{
             return false;
         }
-        //return true;
+
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] > nums[(i + 1) % n]) { 
+        //         cnt++;
+        //     }
+        //     if (cnt > 1){
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 }
