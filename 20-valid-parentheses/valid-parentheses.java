25dd86f9f621aeb7aa07d86f082d155fc321b61e
Kavya Chopade <167448474+KavyaChopade@users.noreply.github.com>
2026-05-11 23:36:38 +0530

    Time: 3 ms (87.77%) | Memory: 43.4 MB (25.62%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack=new Stack<>();
-
-        for(int i=0;i<s.length();i++){
-            char cur=s.charAt(i);
-            if(!stack.isEmpty()){
-                char last=stack.peek();
-                if(isPair(last,cur)){
-                    stack.pop();
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char top=st.pop();
+                if((top=='(' && ch==')') ||
+                   (top=='{' && ch=='}') ||
+                   (top=='[' && ch==']')){
                     continue;
                 }
+                else{
+                    return false;
+                }
             }
-            stack.push(cur);
         }
-        return stack.isEmpty();
-    }
-
-    private boolean isPair(char last, char cur){
-        return (last=='(' && cur==')') ||
-               (last=='{' && cur=='}') ||
-               (last=='[' && cur==']');
+        return st.isEmpty();
     }
 }
